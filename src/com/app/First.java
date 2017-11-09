262863f55f4a42ceeb0d2981446c310b5cb2d416
Mahendra <Mahendra@192.168.0.4>
2017-11-09 22:52:43 +0530

    deleted commented doPost()

--- src/com/app/First.java
+++ src/com/app/First.java
@@ -32,9 +32,6 @@ public class First extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		doGet(request, response);
-	}
+
 
 }
